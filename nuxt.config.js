ece34f0a588e754f2e9a183635dd3e04ddbc5da4
themanthan9 <manthanthummar9@gmail.com>
2021-05-12 11:12:21 +0530

    New line Issue done

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,20 +4,20 @@ export default {
 		title: 'twizz',
 		meta: [
 			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: '' }
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{ hid: 'description', name: 'description', content: '' },
 		],
-		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-		]
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: ["~assets/scss/main.scss"],
+	css: ['~assets/scss/main.scss'],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-	plugins: [
-	],
+	plugins: [],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
 	components: true,
@@ -42,15 +42,14 @@ export default {
 	// PWA module configuration: https://go.nuxtjs.dev/pwa
 	pwa: {
 		manifest: {
-			lang: 'en'
-		}
+			lang: 'en',
+		},
 	},
 
 	// Build Configuration: https://go.nuxtjs.dev/config-build
-	build: {
-	},
+	build: {},
 
 	tailwindcss: {
-		viewer: false
-	}
-}
+		viewer: false,
+	},
+};
