d7db1695235b622b3a306d31b7d30e6274693d7a
themanthan9 <manthanthummar9@gmail.com>
2021-05-14 21:16:24 +0530

    Crizz

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>123</div>
+	<div>
+		<!-- banner section  -->
+		<section class="banner">
+			<div class="container mx-auto">
+				<div class="grid grid-cols-2 gap-2 h-screen items-center">
+					<div class="">
+						<div class="banner-text">
+							<h1 class="font-semibold text-4xl leading-loose">
+								Get Your<br />
+								<span class="text-6xl font-bold"
+									>Desire Food </span
+								><br />
+								in 30 Minutes
+							</h1>
+							<p>
+								Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Voluptas sequi assumenda,
+							</p>
+						</div>
+					</div>
+					<div class="">
+						<div
+							class="banner-img w-3/4 ml-auto rounded-l-3xl rounded-r-3xl"
+						>
+							<img
+								src="../assets/images/banner.jpg"
+								class="ml-auto rounded-l-3xl rounded-r-3xl"
+								alt=""
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- banner section end  -->
+	</div>
 </template>
 
 <script>
-export default {}
+export default {
+	layout: 'weblayout',
+};
 </script>
 
 <style lang="scss" scoped>
